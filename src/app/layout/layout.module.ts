144476112fe420db23a65fbd9b2bcdb4db49548d
Mykhailo Vovk <flyingsteps11@gmail.com>
2023-03-19 16:49:25 +0100

    initial commit

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { RouterOutlet } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, FooterComponent, WrapperComponent],
+  imports: [CommonModule, RouterOutlet],
+  exports: [WrapperComponent],
 })
 export class LayoutModule {}
